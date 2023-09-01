d89368d3839e94eae3fe0469c10fec60babed2e2
HAC <harshchauhan291@gmail.com>
2023-09-01 11:24:40 +0530

    bug fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,10 +40,11 @@ const ProjectCard = ({
       {active !== id ? (
         <div className="flex items-center justify-start pr-[4.5rem]">
           <h3
-            className="font-extrabold font-beckman uppercase w-[200px] h-[30px] 
+            className="font-extrabold font-beckman uppercase w-[200px] h-[30px] l-[-100px]
         whitespace-nowrap sm:text-[27px] text-[18px] text-timberWolf tracking-[1px]
         absolute z-0 lg:bottom-[7rem] lg:rotate-[-90deg] lg:origin-[0,0]
         leading-none z-20"
+            style={{ marginLeft: "-100px" }}
           >
             {name}
           </h3>
